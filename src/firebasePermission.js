49801733f17982e68c242d4090ca42dd92fd2382
cheenaeng <cheena94sing@gmail.com>
2022-05-10 09:35:43 +0800

    added firebase files back

---
+++ src/firebasePermission.js
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getMessaging, getToken } from 'firebase/messaging';
+import 'regenerator-runtime/runtime';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA5UvElGUj9orm_W-J15iXzYLQFyKMl710',
+  authDomain: 'project4-jyl.firebaseapp.com',
+  projectId: 'project4-jyl',
+  storageBucket: 'project4-jyl.appspot.com',
+  messagingSenderId: '36032499854',
+  appId: '1:36032499854:web:748c4736edccefcdbc79df',
+  measurementId: 'G-JHGZGFMDEJ',
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const messaging = getMessaging();
+
+export async function getFCMToken() {
+  try {
+    const token = await getToken(messaging, { vapidKey: 'BPo0Dv-gHXRlg9TMOOTaVdt8z0EHe4FXtJR7xaQI1PIIHFngPmEycMwidTdTPMD991vVqCpebBJPr_Xwxg-ThDU' });
+    return token;
+  } catch (e) {
+    console.log('getFCMToken error', e);
+    return undefined;
+  }
+}
